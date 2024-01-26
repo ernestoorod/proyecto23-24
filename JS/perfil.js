a0a67824d8e248fe235137db82d2a6ba574ac36f
Ernesto Rodriguez Rodriguez <ernessttooo16@gmail.com>
2024-01-26 10:44:20 +0100

    26/01/2024 10:44

--- JS/perfil.js
+++ JS/perfil.js
@@ -1,7 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const btnAbrirModal = document.getElementById('btn-abrir-modal');
     const btnCerrarModal = document.getElementById('btn-cerrar-modal');
-    const modal = document.getElementById('modal');
+    const btnVolverModal = document.getElementById('btn-volver-modal');
+    const btnCerrarCrearCarrera = document.getElementById('btn-cerrar-crearcarrera');
+    const btnVolverCrearCarrera = document.getElementById('btn-volver-crearcarrera');
+    const nombreCarreraSpan = document.getElementById('nombreCarreraSpan');
 
     btnAbrirModal.addEventListener("click", () => {
         modal.showModal();
@@ -9,5 +12,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
     btnCerrarModal.addEventListener("click", () => {
         modal.close();
+    });
+
+    btnVolverModal.addEventListener("click", () => {
+        modal.close();
+    });
+
+    btnCerrarCrearCarrera.addEventListener("click", () => {
+        crearcarrera.close();
+    });
+
+    btnVolverCrearCarrera.addEventListener("click", () => {
+        crearcarrera.close();
+        modal.showModal();
+    });
+
+    const btnSiguiente = document.getElementById('btn-siguiente');
+
+    btnSiguiente.addEventListener("click", () => {
+        const nombreCarrera = document.getElementById('nombrecarrera').value;
+        nombreCarreraSpan.textContent = nombreCarrera;
+        modal.close();
+        crearcarrera.showModal();
     });
 });
